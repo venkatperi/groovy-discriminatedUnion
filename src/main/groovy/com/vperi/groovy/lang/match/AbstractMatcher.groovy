cce136e2c71dca4cb8d9acb183a3edb3c797c62d
venkat peri <venkatperi@gmail.com>
2013-08-11 08:39:22 -0400

    updated license

--- src/main/groovy/com/vperi/groovy/lang/match/AbstractMatcher.groovy
+++ src/main/groovy/com/vperi/groovy/lang/match/AbstractMatcher.groovy
@@ -1,12 +1,24 @@
+/*
+ * Copyright (c) 2013 Venkat Peri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.vperi.groovy.lang.match
 
 import com.vperi.groovy.lang.Variable
 import org.apache.log4j.Logger
 
-/**
- * Copyright (c) 2012-13 by Author(s). All Right Reserved.
- * Author(s): venkat
- */
 @SuppressWarnings("GroovyUnusedDeclaration")
 abstract class AbstractMatcher extends Resolver implements Matcher {
   static LOG = Logger.getLogger( AbstractMatcher )
